486ab1f3191f00b7660021829cf4bfbf22ce64d3
Raymond Cox <raymond@helios.raymondjcox.com>
2016-01-12 23:11:29 -0500

    Fix initializer name
    !!!

--- app/initializers/lazy-video.js
+++ app/initializers/lazy-video.js
@@ -1,7 +1,7 @@
 import LazyVideoProviders from 'ember-lazy-video/services/lazy-video-providers';
 
 export default {
-  name: 'flash-messages',
+  name: 'lazy-video',
   initialize: function(container, application){
     application.register('service:lazy-video-providers', LazyVideoProviders, { singleton: true });
     application.inject('component:lazy-video', 'providers', 'service:lazy-video-providers');
